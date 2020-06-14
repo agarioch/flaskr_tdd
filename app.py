da28f7e46c1ef8666b3f9ab8d1b37f64bb93d380
agarioch <alistairgarioch@gmail.com>
2020-06-14 12:28:16 +0100

    Initial commit with app template, db setup and application tests

---
+++ app.py
@@ -0,0 +1,59 @@
+import sqlite3
+from flask import (
+    Flask,
+    request,
+    session,
+    g,
+    redirect,
+    url_for,
+    abort,
+    render_template,
+    flash,
+    jsonify,
+)
+
+# Configuration
+DATABASE = "flaskr.db"
+DEBUG = True
+SECRET_KEY = "Alistair"
+USERNAME = "admin"
+PASSWORD = "admin"
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+def connect_to_db():
+    rv = sqlite3.connect(app.config["DATABASE"])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource("schema.sql", mode="r") as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+def get_db():
+    if not hasattr(g, "sqlite_db"):
+        g.sqlite_db = connect_to_db()
+    return g.sqlite_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, "sqlite_db"):
+        g.sqlite_db.close()
+
+
+@app.route("/")
+def hello_world():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    init_db()
+    app.run()
